84767a8ced6d8aa9606a224504de6fc4df0cc68b
VitorLinas <103148031+VitorLinas@users.noreply.github.com>
2022-08-07 19:31:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const cabecalho = document.querySelector('header');
 const tweetar = document.querySelector('button');
 const textarea = document.querySelector('textarea');
-const feed = document.querySelector('.conteudoPrincipal_listaTweets')
+const feed = document.querySelector('.conteudoPrincipal_listaTweets');
+
 
 function pegarTweet(event) {
     event.preventDefault(); //previne que o evento padrão do formulario (tentar enviar para algum lugar) ocorra.
     const tweetTextarea = textarea.value;
     criarTweet(tweetTextarea);
+    // circleWhite;
+    // circleBlue;
+    // circleBlack;
 }
 
 tweetar.addEventListener('click', pegarTweet);
@@ -18,21 +22,28 @@ function criarTweet(tweetTexto){
     let hora = data.getHours();
     let minutos = data.getMinutes();
 
+    let interation = document.getElementsByClassName("botoesDeinteracao")
+
     const tweet = {
         nome: "Twitter",
         foto: "imagens/perfilTwitter.png",
-        usuario: "@twitter",
+        usuario: "@Twitter",
         texto: tweetTexto,
         tempo: `${hora}:${minutos}`,
-        // interacao: "oi"
-    } 
 
+        comentario:"imagens/comentar.SVG",
+        retweet:"imagens/retweetar.SVG" ,
+        like:"imagens/like.SVG",
+        compartilhar:"imagens/compartilhar.SVG"
+        // interacao: interation
+    } 
+    console.log(tweet.interacao);
     listarTweet(tweet);
 }
 
 function listarTweet(tweet){
 
-    const {nome, foto, usuario, texto, tempo, } = tweet;
+    const {nome, foto, usuario, texto, tempo, comentario, retweet, like, compartilhar} = tweet;
 
     let li   = document.createElement("li");
     li.classList.add("conteudoPrincipal_tweet");
@@ -50,16 +61,36 @@ function listarTweet(tweet){
     let span = document.createElement("span");
     span.innerText = `${usuario} . ${tempo}`
 
-    let p    = document.createElement("p");
+    let p = document.createElement("p");
     p.innerText = texto;
 
-    // let interacao = document.createElement("div");
-    // div.classList.add("botoesDeinteraçao");
+    // p.style.color = 'red';
+
+
+    let section = document.createElement("section");
+    section.classList.add("botoesDeinteracao")
+    // console.log(section);
 
     
 
+    let coment = document.createElement("img");
+    coment.src = comentario;
+    coment.classList.add("bdi");
+    // console.log(coment);
 
+    let retwe = document.createElement("img");
+    retwe.src = retweet;
+    retwe.classList.add("bdi");
 
+    let lik = document.createElement("img");
+    lik.src = like;
+    lik.classList.add("bdi");
+
+    let comp = document.createElement("img");
+    comp.src = compartilhar;
+    comp.classList.add("bdi");
+
+  
     div.appendChild(h2);
     div.appendChild(span);
     div.appendChild(p);
@@ -70,5 +101,128 @@ function listarTweet(tweet){
     feed.appendChild(li);
     textarea.value = "";
 
-    div.appendChild(interacao);
+    li.appendChild(section);
+    div.appendChild(section)
+    section.appendChild(coment);
+    section.appendChild(retwe);
+    section.appendChild(lik);
+    section.appendChild(comp);
+    console.log(li);
+
+
+    // p.style.color ='white';
+   
+    // if(circleWhite === true){
+    //     p.style.color = 'blue';
+    // }else{
+    //     p.style.color - 'red'
+    // }
+}  
+    
+
+
+
+function circleWhite(){ 
+    console.log("CIRCLEWHITE");
+    
+    // location. reload();
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+
+    let palavras = document.querySelectorAll("a, .buscaTwitter input, .conteudoPrincipal_formulario textarea");
+    for (let index = 0; index < palavras.length; index++) {
+      palavras[index].style.color = "black"; 
+    }
+
+    let tweetP = document.querySelectorAll(".tweet_conteudo p");
+    for (let index = 0; index < tweetP.length; index++) {
+      tweetP[index].style.color = "#333333"; 
+    }
+
+    let palavrasPequenas = document.querySelectorAll(".containerDeAssuntos p, .termosTwitter");
+    for (let index = 0; index < palavrasPequenas.length; index++) {
+        palavrasPequenas[index].style.color = "rgb(87, 86, 86)"; 
+    }
+
+    let palavrasPequenash3 = document.querySelectorAll(".containerDeAssuntos h3");
+        for (let index = 0; index < palavrasPequenash3.length; index++) {
+            palavrasPequenash3[index].style.color = "black"; 
+    }
+
+    let bordas = document.querySelectorAll(".conteudoPrincipal, .conteudoPrincipal_formulario, .conteudoPrincipal_tweet");
+        for (let index = 0; index < bordas.length; index++) {
+        bordas[index].style.borderColor = "#e7ecf0"; 
+    }
+
+    let fundoBusca =  document.querySelectorAll(".buscaTwitter");
+        for (let index = 0; index < fundoBusca.length; index++) {
+        fundoBusca[index].style.backgroundColor = "rgb(228, 236, 236)"; 
+    }
+
+    let fundoassuntos =  document.querySelectorAll(".containerDeAssuntos");
+    for (let index = 0; index < fundoassuntos.length; index++) {
+    fundoassuntos[index].style.backgroundColor = "rgb(242, 246, 246)"; 
+    }
 }
+
+
+
+function circleBlue(){
+    console.log("CIRCLEBLUE");
+    document.body.style.backgroundColor = 'rgb(21, 32, 43)';
+    document.body.style.color = 'white';
+    // p.style.color = 'white'
+    
+
+
+    let palavras = document.querySelectorAll("a, .tweet_conteudo p, .buscaTwitter input, .conteudoPrincipal_formulario textarea");
+    for (let index = 0; index < palavras.length; index++) {
+      palavras[index].style.color = "white"; 
+    }
+
+    let palavrasPequenas = document.querySelectorAll(".containerDeAssuntos p, .containerDeAssuntos h3, .termosTwitter");
+    for (let index = 0; index < palavrasPequenas.length; index++) {
+        palavrasPequenas[index].style.color = "rgb(112,124,137)"; 
+    }
+
+    let bordas = document.querySelectorAll(".conteudoPrincipal, .conteudoPrincipal_formulario, .conteudoPrincipal_tweet");
+        for (let index = 0; index < bordas.length; index++) {
+        bordas[index].style.borderColor = "rgb(56,68,77)"; 
+    }
+
+    let fundo =  document.querySelectorAll(".buscaTwitter, .containerDeAssuntos");
+        for (let index = 0; index < fundo.length; index++) {
+        fundo[index].style.backgroundColor = "rgb(30,39,50)"; 
+    }
+}  
+
+
+
+
+function circleBlack(){
+    console.log("CIRCLEBLACK");
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+
+    let palavras = document.querySelectorAll("a, .tweet_conteudo p, .buscaTwitter input, .conteudoPrincipal_formulario textarea");
+        for (let index = 0; index < palavras.length; index++) {
+          palavras[index].style.color = "white"; 
+    }
+
+    let palavrasPequenas = document.querySelectorAll(".containerDeAssuntos p, .containerDeAssuntos h3, .termosTwitter");
+    for (let index = 0; index < palavrasPequenas.length; index++) {
+        palavrasPequenas[index].style.color = "rgb(84,88,96)"; 
+    }
+
+
+    let bordas = document.querySelectorAll(".conteudoPrincipal, .conteudoPrincipal_formulario, .conteudoPrincipal_tweet");
+        for (let index = 0; index < bordas.length; index++) {
+        bordas[index].style.borderColor = "rgb(47,51,54)"; 
+    }
+
+    let fundo =  document.querySelectorAll(".buscaTwitter, .containerDeAssuntos");
+        for (let index = 0; index < fundo.length; index++) {
+        fundo[index].style.backgroundColor = "rgb(22,24,28)"; 
+    }
+}
+
